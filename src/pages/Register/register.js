cc9bfcc8b8d5f34e8d195107ef8eb61b10a2f834
Jalapina <davidpina14@gmail.com>
2022-11-11 03:36:11 -0600

    files name change

--- src/pages/Register/register.js
+++ src/pages/Register/register.js
@@ -8,8 +8,6 @@ const Register = () =>{
             <form action="">
               <input type="text" className="ghost-input" placeholder="Email" required/> 
               <input type="text" className="ghost-input" placeholder="Username" required/>
-              <input type="password" className="ghost-input" placeholder="Password" required/>
-              <input type="password" className="ghost-input" placeholder="Confirm Password" required/>
               <input type="submit" className="ghost-button"/>
             </form>
         </div>
