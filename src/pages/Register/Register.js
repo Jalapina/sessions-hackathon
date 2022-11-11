e8fcc0c3b83d48f3825a54882916f28964b911ea
David Pina <davidpina14@gmail.com>
2022-11-11 03:41:34 -0600

    Created Register js

---
+++ src/pages/Register/Register.js
@@ -0,0 +1,20 @@
+import React, {useState,useEffect,useRef} from 'react';
+import "./register.css"
+
+const Register = () =>{
+    return(
+        <div className="register">
+        <div className="container">
+            <form action="">
+              <input type="text" className="ghost-input" placeholder="Email" required/> 
+              <input type="text" className="ghost-input" placeholder="Username" required/>
+              <input type="submit" className="ghost-button"/>
+            </form>
+        </div>
+        </div>
+
+    )
+}
+    
+
+export default Register
